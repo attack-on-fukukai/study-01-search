3d342c2fcc3b6d7575977e6a14600bc177c40701
attack-on-fukukai <74611435+attack-on-fukukai@users.noreply.github.com>
2020-12-30 23:17:14 +0900

    Update search.py

--- search.py
+++ search.py
@@ -9,30 +9,23 @@ def readCsv(faileName):
         wrk_list.append(item)
     return wrk_list
 
-### 文字比較関数
-def matching(word,source):
-    for item in source:
-        if item == word:
-            return True
-    return False
 
 ### 検索ツール
 def search(word,sourceList):  
     # 入力されたワードとリストを比較する
-    if matching(word,sourceList) == True:
-        print("{}が見つかりした".format(word))
+    if word in sourceList:
+        print(f"{word}が見つかりした")
     else:
-        print("{}が見つかりませんでした\n見つからなかったのでリストに追加します".format(word))
+        print(f"{word}が見つかりませんでした\n見つからなかったのでリストに追加します")
         sourceList.append(word)        
 
     df = pd.DataFrame(sourceList)
     df.to_csv("out.csv",header=False)
 
-if __name__ == "__main__":
 
-    sourceList = readCsv("source.csv")
+if __name__ == "__main__":
+    
     word =input("鬼滅の登場人物の名前を入力してください >>> ")
 
+    sourceList = readCsv("source.csv")
     search(word,sourceList)
-
-
